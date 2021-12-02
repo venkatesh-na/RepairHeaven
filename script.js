30753d900bdeea369351bc97a7dd66f61888d888
venkatesh-na <79442047+venkatesh-na@users.noreply.github.com>
2021-12-02 21:16:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 const divOfInnerTest = document.querySelectorAll(".testimonial .inner-testimonies > div")
 const homeSliderDiv = document.querySelectorAll(".home main article")
 //608px
-console.log(divOfInnerTest)
 const testPrev = document.querySelector(".testimonial .slide .prev") 
 const homePrev = document.querySelector(".home .home-slide .prev")
 const homeNext = document.querySelector(".home .home-slide .next")
 const testNext = document.querySelector(".testimonial .slide .next")
 const bar = document.querySelector(".home header > div i")
 const homeUl = document.querySelector(".home header ul")
-console.log(testPrev,testNext)
 const mainh1 = document.querySelectorAll(".home main h1")
 const mainh4 = document.querySelectorAll(".home main h4")
 const maina = document.querySelectorAll(".home main a")
@@ -61,17 +59,17 @@ document.addEventListener("DOMContentLoaded",()=>{
     pageAnimation();
 })
 document.addEventListener("scroll",(e)=>{
-    console.log(window.pageYOffset)
     if(window.pageYOffset > 90)
     {
         homeHeader.style.backgroundColor = "white"
         homeHeaderh1.style.color = "black"
-
+        homeHeader.style.boxShadow = "3px 3px 5px grey"
     }
     else
     {
         homeHeader.style.backgroundColor = "transparent"
         homeHeaderh1.style.color = "white"
+        homeHeader.style.boxShadow = "none"
     }
 })
 function testSlideLeft()
@@ -91,16 +89,12 @@ function homeSlideLeft()
     homeSliderDiv.forEach((e)=>{
     e.style.transform = "translateX(-100%)"
     })
-    
-   
 } 
 function homeSlideRight()
 {
     homeSliderDiv.forEach((e)=>{
     e.style.transform = "translateX(0px)"
     })
-   
-    
 }
 function toggleLink()
 {
